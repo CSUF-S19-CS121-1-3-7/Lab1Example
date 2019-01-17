06cba902321aac43d0ece95420e19712331c4f69
Eric May <yaromesky@gmail.com>
2019-01-17 09:44:19 -0800

    Create MayELab1.cpp

---
+++ MayELab1.cpp
@@ -0,0 +1,14 @@
+//Eric May
+//Spring 2019
+//Lab 1
+
+//This is Lab 1, but with Hello World
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!";
+    return 0;
+}
